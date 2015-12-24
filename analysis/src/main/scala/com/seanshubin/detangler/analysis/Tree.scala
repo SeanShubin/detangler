b31a80dfeb6e908799628af7d60c56e4ac9ee2de
SeanShubin <seanshubin@gmail.com>
2015-12-23 23:08:17 -0800

    Added new tree implementation

---
+++ analysis/src/main/scala/com/seanshubin/detangler/analysis/Tree.scala
@@ -0,0 +1,62 @@
+package com.seanshubin.detangler.analysis
+
+case class Tree[T](value: T, path: Seq[String] = Seq(), branches: Seq[Tree[T]] = Seq()) {
+
+  import Tree._
+
+  def add(pathToAdd: Seq[String]): Tree[T] = {
+    if (path == pathToAdd) {
+      this
+    } else if (isSuperPath(path, pathToAdd)) {
+      val immediateBranchKeys = branches.map(_.path)
+      val branchToAdd = pathToAdd.take(path.size + 1)
+      val index = immediateBranchKeys.indexOf(branchToAdd)
+      if (index == -1) {
+        val newEmptyBranch = new Tree(value, branchToAdd)
+        val newBranch = newEmptyBranch.add(pathToAdd)
+        val newBranches = branches :+ newBranch
+        copy(branches = newBranches)
+      } else {
+        val existingBranch = branches(index)
+        val newBranch = existingBranch.add(pathToAdd)
+        val newBranches = branches.updated(index, newBranch)
+        copy(branches = newBranches)
+      }
+    } else {
+      throw new RuntimeException(s"$path is not a super path of $pathToAdd")
+    }
+  }
+
+  def keys(): Seq[Seq[String]] = {
+    path +: branches.flatMap(_.keys())
+  }
+
+  def map[U](f: (T, Seq[String]) => U): Tree[U] = {
+    new Tree[U](f(value, path), path, branches.map(_.map(f)))
+  }
+
+  def value(valuePath: Seq[String]): T = {
+    if (path == valuePath) {
+      value
+    } else if (isSuperPath(path, valuePath)) {
+      val immediateBranchKeys = branches.map(_.path)
+      val nextChildDown = valuePath.take(path.size + 1)
+      val index = immediateBranchKeys.indexOf(nextChildDown)
+      if (index == -1) {
+        throw new RuntimeException(s"No value found at $valuePath")
+      } else {
+        branches(index).value(valuePath)
+      }
+    } else {
+      throw new RuntimeException(s"$path is not a super path of $valuePath")
+    }
+  }
+}
+
+object Tree {
+  val Empty = new Tree[Unit](Unit)
+
+  def isSuperPath(left: Seq[String], right: Seq[String]): Boolean = {
+    left == right.take(left.size)
+  }
+}
---
+++ analysis/src/test/scala/com/seanshubin/detangler/analysis/TreeTest.scala
@@ -0,0 +1,55 @@
+package com.seanshubin.detangler.analysis
+
+import org.scalatest.FunSuite
+
+class TreeTest extends FunSuite {
+  val root = Seq()
+  val groupA = Seq("group/a")
+  val groupB = Seq("group/b")
+  val packageC = Seq("group/a", "package/c")
+  val packageD = Seq("group/a", "package/d")
+  val packageE = Seq("group/b", "package/e")
+  val classF = Seq("group/a", "package/c", "class/f")
+  val classG = Seq("group/a", "package/c", "class/g")
+  val classH = Seq("group/a", "package/d", "class/h")
+  val classI = Seq("group/b", "package/e", "class/i")
+
+  test("construct tree") {
+    val tree = Tree.Empty.add(classF).add(classG).add(classH).add(classI)
+    val keys = tree.keys()
+    keys.foreach(println)
+    assert(keys.size === 10)
+    assert(keys(0) === root)
+    assert(keys(1) === groupA)
+    assert(keys(2) === packageC)
+    assert(keys(3) === classF)
+    assert(keys(4) === classG)
+    assert(keys(5) === packageD)
+    assert(keys(6) === classH)
+    assert(keys(7) === groupB)
+    assert(keys(8) === packageE)
+    assert(keys(9) === classI)
+  }
+
+  test("map function over tree") {
+    val tree = Tree.Empty.add(classF).add(classG).add(classH).add(classI)
+    def composeName(value: Unit, path: Seq[String]): String = {
+      def modifyPathPart(pathPart: String): String = {
+        pathPart.dropWhile(_ != '/').tail
+      }
+      path.map(modifyPathPart).mkString("(", "", ")")
+    }
+    val actual = tree.map(composeName)
+    assert(actual.value(root) === "()")
+    assert(actual.value(groupA) === "(a)")
+    assert(actual.value(groupB) === "(b)")
+    assert(actual.value(packageC) === "(ac)")
+    assert(actual.value(packageD) === "(ad)")
+    assert(actual.value(packageE) === "(be)")
+    assert(actual.value(classF) === "(acf)")
+    assert(actual.value(classG) === "(acg)")
+    assert(actual.value(classH) === "(adh)")
+    assert(actual.value(classI) === "(bei)")
+  }
+
+}
