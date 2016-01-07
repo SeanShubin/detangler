df3ca92095aaab521eb14cc041ea2070d2b75fba
Sean Shubin <sshubin@cj.com>
2016-01-06 18:37:33 -0800

    link displays how many parts a module is composed of

--- report/src/main/scala/com/seanshubin/detangler/report/StandaloneSummaryTemplateRulesImpl.scala
+++ report/src/main/scala/com/seanshubin/detangler/report/StandaloneSummaryTemplateRulesImpl.scala
@@ -4,13 +4,19 @@ import com.seanshubin.detangler.model.{Detangled, Standalone}
 
 class StandaloneSummaryTemplateRulesImpl(detangled: Detangled) extends StandaloneSummaryTemplateRules {
   override def generate(summaryTemplate: HtmlElement, standalone: Standalone): HtmlElement = {
+    val partCount = detangled.childModules(standalone).size
+    val partString = if (partCount == 1) {
+      "1 part"
+    } else {
+      s"$partCount parts"
+    }
     cycleLink(summaryTemplate, standalone).
       attr(".standalone-summary", "id", HtmlRendering.htmlId(standalone)).
       text(".name", HtmlRendering.htmlName(standalone)).
       text(".depth", detangled.depth(standalone).toString).
       text(".breadth", detangled.breadth(standalone).toString).
       text(".transitive", detangled.transitive(standalone).toString).
-      anchor(".composed-of", HtmlRendering.reportFile(standalone), "parts")
+      anchor(".composed-of", HtmlRendering.reportFile(standalone), partString)
   }
 
   private def cycleLink(template: HtmlElement, standalone: Standalone): HtmlElement = {
