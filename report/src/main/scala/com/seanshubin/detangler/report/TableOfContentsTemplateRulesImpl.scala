bbee1e40829b2c18af95f033df212c42c7f4e110
Sean Shubin <sshubin@cj.com>
2018-10-04 16:11:04 -0700

    Broke table of contents into levels

--- report/src/main/scala/com/seanshubin/detangler/report/TableOfContentsTemplateRulesImpl.scala
+++ report/src/main/scala/com/seanshubin/detangler/report/TableOfContentsTemplateRulesImpl.scala
@@ -7,11 +7,12 @@ import scala.annotation.tailrec
 class TableOfContentsTemplateRulesImpl(detangled: Detangled) extends TableOfContentsTemplateRules {
   override def generate(template: HtmlElement): HtmlElement = {
     val empty = template.remove(".table-of-contents-row")
-    val rowTemplate = template.select(".table-of-contents-row")
+    val rowTemplate = template.select(".table-of-contents-row").remove(".table-of-contents-cell")
+    val cellTemplate = template.select(".table-of-contents-cell")
 
-    def appendChild(appendToMe: HtmlElement, child: Standalone): HtmlElement = {
-      val text = standaloneToText(child)
-      val row = rowTemplate.anchor(".link", HtmlRender.absoluteModuleLink(child), text)
+    def appendChild(appendToMe: HtmlElement, standalone: Standalone): HtmlElement = {
+      val cells = standaloneToCells(cellTemplate, standalone)
+      val row = rowTemplate.append(".table-of-contents-row", cells)
       appendToMe.append("#table-of-contents-body", row)
     }
 
@@ -19,17 +20,20 @@ class TableOfContentsTemplateRulesImpl(detangled: Detangled) extends TableOfCont
     children.foldLeft(empty)(appendChild)
   }
 
-  private def standaloneToText(standalone: Standalone): String = {
-    standaloneToTextRecursive(Nil, standalone)
+
+  private def standaloneToCells(cellTemplate:HtmlElement, standalone: Standalone): Seq[HtmlElement] = {
+    standaloneToCellsRecursive(Nil, cellTemplate, standalone)
   }
 
   @tailrec
-  private def standaloneToTextRecursive(soFar: List[String], standalone: Standalone): String = {
+  private def standaloneToCellsRecursive(soFar: List[HtmlElement], cellTemplate:HtmlElement, standalone: Standalone): Seq[HtmlElement] = {
     if (standalone.isRoot) {
-      soFar.mkString(".")
+      soFar
     } else {
       val text = HtmlRender.reportPageLinkName(standalone)
-      standaloneToTextRecursive(text :: soFar, standalone.parent)
+      val href = HtmlRender.absoluteModuleLink(standalone)
+      val cell = cellTemplate.anchor(".link", href, text)
+      standaloneToCellsRecursive(cell :: soFar, cellTemplate, standalone.parent)
     }
   }
 }
