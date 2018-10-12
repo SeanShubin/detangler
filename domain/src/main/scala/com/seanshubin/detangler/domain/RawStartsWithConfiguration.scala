6dfb7b4e1d0213bc13c89e2b88a6f28d4705d45c
Sean Shubin <sshubin@cj.com>
2018-10-12 13:24:32 -0700

    refactoring

--- domain/src/main/scala/com/seanshubin/detangler/domain/RawStartsWithConfiguration.scala
+++ domain/src/main/scala/com/seanshubin/detangler/domain/RawStartsWithConfiguration.scala
@@ -3,10 +3,10 @@ package com.seanshubin.detangler.domain
 case class RawStartsWithConfiguration(include: Option[Seq[Seq[String]]],
                                    exclude: Option[Seq[Seq[String]]],
                                    drop: Option[Seq[Seq[String]]]) {
-  def replaceEmptyWithDefaults(): ValidatedStartsWithConfiguration = {
-    val newInclude = include.getOrElse(ValidatedStartsWithConfiguration.Default.include)
-    val newExclude = exclude.getOrElse(ValidatedStartsWithConfiguration.Default.exclude)
-    val newDrop = drop.getOrElse(ValidatedStartsWithConfiguration.Default.drop)
-    ValidatedStartsWithConfiguration(newInclude, newExclude, newDrop)
+  def replaceEmptyWithDefaults(): StartsWithConfiguration = {
+    val newInclude = include.getOrElse(StartsWithConfiguration.Default.include)
+    val newExclude = exclude.getOrElse(StartsWithConfiguration.Default.exclude)
+    val newDrop = drop.getOrElse(StartsWithConfiguration.Default.drop)
+    StartsWithConfiguration(newInclude, newExclude, newDrop)
   }
 }
